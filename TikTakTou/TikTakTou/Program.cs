32218b5d5835527be509e19f2aebf68672048f22
atrokos <honza.pijalek@seznam.cz>
2022-04-09 09:18:03 +0200

    TikTakTou: Added minimax

--- TikTakTou/TikTakTou/Program.cs
+++ TikTakTou/TikTakTou/Program.cs
@@ -10,50 +10,29 @@ namespace tictactoe
         {
             Player P1, P2;
             Board board;
-            Checker checker;
 
-            public TurnManager(Player p1, Player p2, Board board, Checker checker)
+            public TurnManager(Player p1, Player p2, Board board)
             {
                 P1 = p1;
                 P2 = p2;
                 this.board = board;
-                this.checker = checker;
-            }
-        }
-
-        class Checker // Checks if any of the two players won
-        {
-            Player P1, P2;
-            Board board;
-
-            public Checker(Player p1, Player p2, Board board)
-            {
-                P1 = p1;
-                P2 = p2;
-                this.board = board;
-            }
-
-            public void CheckBoard()
-            {
-                for (int i = 0; i < board.GetHeight(); i++)
-                {
-                    if (board.GetTile(i, 0) == board.GetTile(i, 1) && board.GetTile(i, 1) == board.GetTile(i, 2))
-                    {
-
-                    }
-                }
             }
         }
 
         class Player
         {
             string sign; // X or O
+            string opponent; // Opponent's sign
             bool isAI; // Whether it is controlled by the AI
 
             public Player(string sign, bool isAI)
             {
                 this.isAI = isAI;
                 this.sign = sign;
+                if (sign == "x")
+                    this.opponent = "o";
+                else
+                    this.opponent = "x";
             }
 
             public string Sign()
@@ -61,15 +40,70 @@ namespace tictactoe
                 return sign;
             }
 
-            public void YourTurn()
+            public string Opponent()
             {
-
-            } 
+                return opponent;
+            }
         }
 
         class AI
         {
+            Player player;
+            Board board;
 
+            int startMinimax()
+            {
+                Board evalboard = new Board(board.GetWholeBoard());
+
+            }
+
+            int minimax(Board evalboard, bool Max) // Evaluate evalboard; Max = true for max's turn, false for min's turn
+            {
+                int currentValue = evalboard.EvaluateBoard(player.Sign());
+                if (currentValue == 10)
+                    return currentValue;
+
+                if (currentValue == -10)
+                    return currentValue;
+
+                if (!evalboard.AnyMovesLeft())
+                    return 0;
+
+
+                if (Max)
+                {
+                    int bestMove = -100;
+                    for (int i = 0; i < evalboard.GetHeight(); i++)
+                    {
+                        for (int j = 0; j < evalboard.GetWidth(); j++)
+                        {
+                            if (evalboard.GetTile(i,j) == ".") // If the tile is empty, put your sign in it
+                            {
+                                evalboard.ChangeTile(player.Sign(), i, j);
+                                bestMove = Math.Max(bestMove, minimax(evalboard, !Max));
+                                evalboard.ChangeTile(".", i, j); // Remove the sign
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    int bestMove = 100;
+                    for (int i = 0; i < evalboard.GetHeight(); i++)
+                    {
+                        for (int j = 0; j < evalboard.GetWidth(); j++)
+                        {
+                            if (evalboard.GetTile(i, j) == ".") // If the tile is empty, put your opponent's sign in it
+                            {
+                                evalboard.ChangeTile(player.Opponent(), i, j);
+                                bestMove = Math.Min(bestMove, minimax(evalboard, !Max));
+                                evalboard.ChangeTile(".", i, j); // Remove the sign
+                            }
+                        }
+                    }
+                }
+                return 0;
+            }
         }
 
         class Loader
@@ -114,10 +148,38 @@ namespace tictactoe
         class Board
         {
             string[,] board;
+            int NumberOfMoves;
 
-            public Board(string[,] board)
+            public Board(string[,] board) // If created in analytical mode, board is preset
             {
                 this.board = board;
+                NumberOfMoves = 9;
+                for (int i = 0; i < board.GetLength(0); i++) // Updates the number of moves left
+                {
+                    for (int j = 0; j < board.GetLength(1); j++)
+                    {
+                        if (board[i, j] != ".")
+                            NumberOfMoves--;
+                    }
+                }
+            }
+
+            public Board() // If created for interactive play, the board is empty
+            {
+                board = new string[3,3] { { ".", ".", "." }, { ".", ".", "." }, { ".", ".", "." } };
+                NumberOfMoves = 9;
+            }
+
+            public bool AnyMovesLeft()
+            {
+                if (NumberOfMoves == 0)
+                    return false;
+                return true;
+            }
+
+            public string[,] GetWholeBoard()
+            {
+                return board;
             }
 
             public string GetTile(int h, int w)
@@ -144,6 +206,48 @@ namespace tictactoe
                 }
                 return false;
             }
+
+            public int EvaluateBoard(string sign) // Evaluates board for ... tictactoe
+            {
+                for (int i = 0; i < board.GetLength(0); i++) // vertical
+                {
+                    if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                    {
+                        if (board[i, 0] == sign)
+                            return -10;
+                        else
+                            return 10;
+                    }
+                }
+
+                for (int i = 0; i < board.GetLength(1); i++) // horizontal
+                {
+                    if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                    {
+                        if (board[0, i] == sign)
+                            return -10;
+                        else
+                            return 10;
+                    }
+                }
+
+                if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) // diagonal \
+                {
+                    if (board[0, 0] == sign)
+                        return -10;
+                    else
+                        return 10;
+                }
+                else if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0]) // diagonal /
+                {
+                    if (board[0, 2] == sign)
+                        return -10;
+                    else
+                        return 10;
+                }
+                return 0;
+            }
+
         }
 
         static void Main(string[] args)
